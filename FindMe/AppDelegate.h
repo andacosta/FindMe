4539faf69cfe9e6128f89905a55a178aa8762a19
andacosta <iMac@iMac.local>
2015-05-07 20:36:09 -0500

    Initial Commit

---
+++ FindMe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FindMe
+//
+//  Created by Sergio Acosta on 05/07/15.
+//  Copyright (c) 2015 Cappacitate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FindMe/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FindMe
+//
+//  Created by Sergio Acosta on 05/07/15.
+//  Copyright (c) 2015 Cappacitate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
