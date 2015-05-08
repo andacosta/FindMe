ec336a3097030436c43fb0b93051c7e786cef080
andacosta <iMac@iMac.local>
2015-05-08 00:43:55 -0500

    -

--- FindMe/userMapViewController.h
+++ FindMe/userMapViewController.h
@@ -8,7 +8,6 @@
 
 #import <UIKit/UIKit.h>
 #import <MapKit/MapKit.h>
-#import <MessageUI/MFMessageComposeViewController.h>
 
 @interface userMapViewController : UIViewController {
 	MKMapView * userMapView;
--- FindMe/userMapViewController.m
+++ FindMe/userMapViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "userMapViewController.h"
+#import <MessageUI/MessageUI.h>
+#import <MessageUI/MFMessageComposeViewController.h>
 
-@interface userMapViewController ()
+@interface userMapViewController () <MFMailComposeViewControllerDelegate>
 
 @end
 
@@ -44,8 +46,45 @@
 
 
 // Notifications
--(void)sendSMS {
+- (IBAction)sendEmailAction:(UIButton *)sender
+{
+	if ([MFMailComposeViewController canSendMail])
+	{
+		MFMailComposeViewController *mail = [[MFMailComposeViewController alloc] init];
+		mail.mailComposeDelegate = self;
+		[mail setSubject:@"Sample Subject"];
+		[mail setMessageBody:@"Here is some main text in the email!" isHTML:NO];
+		[mail setToRecipients:@[@"testingEmail@example.com"]];
+		
+		[self presentViewController:mail animated:YES completion:NULL];
+	}
+	else
+	{
+		NSLog(@"This device cannot send email");
+	}
+}
+
+- (void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error
+{
+	switch (result) {
+		case MFMailComposeResultSent:
+			NSLog(@"You sent the email.");
+			break;
+		case MFMailComposeResultSaved:
+			NSLog(@"You saved a draft of this email");
+			break;
+		case MFMailComposeResultCancelled:
+			NSLog(@"You cancelled sending this email.");
+			break;
+		case MFMailComposeResultFailed:
+			NSLog(@"Mail failed:  An error occurred when trying to compose this email");
+			break;
+		default:
+			NSLog(@"An error occurred when trying to compose this email");
+			break;
+	}
 	
+	[self dismissViewControllerAnimated:YES completion:NULL];
 }
 
 
